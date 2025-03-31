a697a08a76196d56cb086e0224bde213d88218bc
MaluSevilha <marialssac@al.insper.edu.br>
2025-03-31 04:15:20 -0600

    fazendo pequenos ajustes

--- exe2/main.c
+++ exe2/main.c
@@ -4,130 +4,125 @@
  * SPDX-License-Identifier: BSD-3-Clause
  */
 
- #include <stdio.h>
- #include <string.h> 
- #include "pico/stdlib.h"
- #include "hardware/gpio.h"
- #include "hardware/timer.h"
- #include "hardware/irq.h"
- 
- const int ECHO_PIN_1 = 12;
- const int TRIGGER_PIN_1 = 13;
- 
- volatile int alarm_1 = 0;
- volatile int echo_start_time_1 = 0;
- volatile int echo_end_time_1 = 0;
- 
- const int ECHO_PIN_2 = 18;
- const int TRIGGER_PIN_2 = 19;
- 
- volatile int alarm_2 = 0;
- volatile int echo_start_time_2 = 0;
- volatile int echo_end_time_2 = 0;
- 
- volatile alarm_id_t echo_timeout_alarm_1;
- volatile alarm_id_t echo_timeout_alarm_2;
- 
- float calcula_distancia_cm(uint64_t duracao_us) {
-     // distancia = (duracao * velocidade_som) / 2
-     // v_som = 343 m/s = 34300 cm / 1000000 us = 0.0343 cm/us
-     return (duracao_us * 0.0343) / 2.0;
- }
- 
- int64_t echo_timeout_callback_1(alarm_id_t id, void* user_data) {
-     alarm_1 = 1;
-     return 0;
- }
- 
- int64_t echo_timeout_callback_2(alarm_id_t id, void* user_data) {
-     alarm_2 = 1;
-     return 0;
- }
- 
- void echo_isr(uint gpio, uint32_t events) {
-     if (gpio == ECHO_PIN_1) {
-         if (events & GPIO_IRQ_EDGE_RISE) {
-             echo_start_time_1 = get_absolute_time();
-         }
-         else if (events & GPIO_IRQ_EDGE_FALL) {
-             echo_end_time_1 = get_absolute_time();
-             cancel_alarm(echo_timeout_alarm_1);
-         }
-     }
- 
-     if (gpio == ECHO_PIN_2) {
-         if (events & GPIO_IRQ_EDGE_RISE) {
-             echo_start_time_2 = get_absolute_time();
-         }
-         else if (events & GPIO_IRQ_EDGE_FALL) {
-             echo_end_time_2 = get_absolute_time();
-             cancel_alarm(echo_timeout_alarm_2);
-         }
-     }
- }
- 
- int main() {
-     stdio_init_all();
- 
-     printf("oi \n");
-     gpio_init(ECHO_PIN_1);
-     gpio_init(TRIGGER_PIN_1);
-     gpio_set_dir(ECHO_PIN_1, GPIO_IN);
-     gpio_set_dir(TRIGGER_PIN_1, GPIO_OUT);
-     gpio_put(TRIGGER_PIN_1, 0);
-     gpio_set_irq_enabled_with_callback(ECHO_PIN_1, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &echo_isr);
- 
- 
-     gpio_init(ECHO_PIN_2);
-     gpio_init(TRIGGER_PIN_2);
-     gpio_set_dir(ECHO_PIN_2, GPIO_IN);
-     gpio_set_dir(TRIGGER_PIN_2, GPIO_OUT);
-     gpio_put(TRIGGER_PIN_2, 0);
-     gpio_set_irq_enabled_with_callback(ECHO_PIN_2, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL, true, &echo_isr);
- 
-     while (true) {
-         alarm_1 = 0;
-         echo_start_time_1 = 0;
-         echo_end_time_1 = 0;
- 
-         alarm_2 = 0;
-         echo_start_time_2 = 0;
-         echo_end_time_2 = 0;
- 
-         sleep_us(100);
-         gpio_put(TRIGGER_PIN_1, 1);
-         gpio_put(TRIGGER_PIN_2, 1);
-         sleep_us(10);
-         gpio_put(TRIGGER_PIN_1, 0);
-         gpio_put(TRIGGER_PIN_2, 0);
-         echo_timeout_alarm_1 = add_alarm_in_ms(20, echo_timeout_callback_1, NULL, false);
-         echo_timeout_alarm_2 = add_alarm_in_ms(20, echo_timeout_callback_2, NULL, false);
- 
-         while ((alarm_1 == 0) && (echo_end_time_1 == 0)) {}
- 
-         while ((alarm_2 == 0) && (echo_end_time_2 == 0)) {}
- 
-         if (alarm_1 == 1) {
-             printf("Sensor 1 - dist: erro\n");
-         }
-         else {
-             int dt_1 = echo_end_time_1 - echo_start_time_1;
-             int distancia_1 = (int)((dt_1 * 0.0343) / 2.0);
-             printf("Sensor 1 - dist: %d cm\n", distancia_1);
-         }
- 
-         if (alarm_2 == 1) {
-             printf("Sensor 2 - dist: erro\n");
-         }
-         else {
-             int dt_2 = echo_end_time_2 - echo_start_time_2;
-             int distancia_2 = (int)((dt_2 * 0.0343) / 2.0);
-             printf("Sensor 2 - dist: %d cm\n", distancia_2);
-         }
-         cancel_alarm(echo_timeout_alarm_1);
-         cancel_alarm(echo_timeout_alarm_2);
-     }
- 
-     return 0;
- }
- 
+#include <stdio.h>
+#include <string.h> 
+#include "pico/stdlib.h"
+#include "hardware/gpio.h"
+#include "hardware/timer.h"
+#include "hardware/irq.h"
+
+const int TRIGGER1 = 19;
+const int ECHO1 = 18;
+const int TRIGGER2 = 13;
+const int ECHO2 = 12;
+
+volatile bool timer_fired1 = false;
+volatile bool timer_fired2 = false;
+volatile uint32_t start_us1;
+volatile uint32_t stop_us1; 
+volatile uint32_t start_us2; 
+volatile uint32_t stop_us2;
+
+volatile alarm_id_t alarm1;
+volatile alarm_id_t alarm2;
+
+void pulso_trigger(int TRIGGER){
+    gpio_put(TRIGGER, 1);
+    sleep_us(10);
+    gpio_put(TRIGGER, 0);
+    sleep_us(2);
+}
+
+void echo_callback(uint gpio, uint32_t events) {
+    if (gpio == ECHO1){
+        if (events == 0x4) { 
+            stop_us1 = get_absolute_time();
+        } else if (events == 0x8) { 
+            start_us1 = get_absolute_time();
+            cancel_alarm(alarm1);
+        }
+    } 
+    
+    if (gpio == ECHO2){
+        if (events == 0x4) { 
+            stop_us2 = get_absolute_time();
+        } else if (events == 0x8) { 
+            start_us2 = get_absolute_time();
+            cancel_alarm(alarm2);
+        }
+    }
+}
+
+int64_t alarm1_callback(alarm_id_t id, void *user_data) {
+    timer_fired1 = true;
+    return 0;
+}
+
+int64_t alarm2_callback(alarm_id_t id, void *user_data) {
+    timer_fired2 = true;
+    return 0;
+}
+
+int main() {
+    stdio_init_all();
+
+    gpio_init(TRIGGER1);
+    gpio_set_dir(TRIGGER1, GPIO_OUT);
+    gpio_put(TRIGGER1, 0);
+
+    gpio_init(TRIGGER2);
+    gpio_set_dir(TRIGGER2, GPIO_OUT);
+    gpio_put(TRIGGER2, 0);
+
+    gpio_init(ECHO1);
+    gpio_set_dir(ECHO1, GPIO_IN);
+    gpio_set_irq_enabled_with_callback(ECHO1, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &echo_callback);
+
+    gpio_init(ECHO2);
+    gpio_set_dir(ECHO2, GPIO_IN);
+    gpio_set_irq_enabled_with_callback(ECHO2, GPIO_IRQ_EDGE_FALL | GPIO_IRQ_EDGE_RISE, true, &echo_callback);
+
+    while (true) {
+        int dist1;
+        int dist2;
+
+        timer_fired1 = false;    
+        timer_fired2 = false;
+        stop_us1 = 0;
+        stop_us2 = 0;
+        start_us1 = 0;
+        start_us2 = 0;
+
+        sleep_us(100);
+
+        pulso_trigger(TRIGGER1);
+        pulso_trigger(TRIGGER2);
+
+        alarm1 = add_alarm_in_ms(20, alarm1_callback, NULL, false);
+        alarm2 = add_alarm_in_ms(20, alarm2_callback, NULL, false);
+
+        while(stop_us1 == 0 && timer_fired1 == false){}
+
+        while(stop_us2 == 0 && timer_fired2 == false){}
+
+        if (timer_fired1 == 0){
+            dist1 = (int)((stop_us1 - start_us1)*0.0343)/2;
+            cancel_alarm(alarm1);
+            printf("Sensor 1 - %d cm\n", dist1);
+        } else {
+            printf("Sensor 1 - falha \n");
+        }
+
+        if (timer_fired2 == 0){
+            dist2 = (int)((stop_us2 - start_us2)*0.0343)/2;
+            printf("Sensor 2 - %d cm\n", dist2);
+        } else {
+            printf("Sensor 2 - falha \n");
+        }
+
+        cancel_alarm(alarm1);
+        cancel_alarm(alarm2);
+    }
+
+    return 0;
+}
